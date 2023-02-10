696048c813466214f7cdcc5aa01f4635d43d8fba
Infinity <InfinityShell1337@github.com>
2023-02-09 22:38:52 -0500

    Updated

--- config.el
+++ config.el
@@ -1,8 +1,10 @@
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Infinity"
+      user-mail-address "infinity@1337.com")
 
 (setq doom-theme 'doom-dracula)
 ;(setq doom-theme 'doom-Iosvkem)
+
+(doom/set-frame-opacity 90)
 
 (setq display-line-numbers-type t)
 
